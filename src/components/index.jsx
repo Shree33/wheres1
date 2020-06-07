533c38ced72192db756588f41be34db8fe38e170
Shreenath Bhanderi <skbhanderi3@gmail.com>
2020-06-07 19:23:38 -0400

    Updates

--- src/components/index.jsx
+++
@@ -1,7 +0,0 @@
-import EventList from './EventList';
-import Header from './Header';
-import PostList from './PostList';
-import SEO from './SEO';
-import TagsBlock from './TagsBlock';
-
-export { EventList, Header, PostList, SEO, TagsBlock };
